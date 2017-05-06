5acfeb6e3d52a3e5405880625ccbc330185c5b4c
Eduardo Sztokbant <du@du.net.br>
2017-05-05 22:07:11 -0700

    Adds UIWebView loading PROD URL, refreshing on pull-down

--- ArhaticYogaJournaliOS/ViewController.swift
+++ ArhaticYogaJournaliOS/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+
+    let url = "https://arhaticnet.herokuapp.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.scrollView.delegate = self
+        webView.loadRequest(URLRequest(url: URL(string: url)!))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if (scrollView.contentOffset.y < 0) {
+            webView.reload()
+        }
+    }
 }
-
