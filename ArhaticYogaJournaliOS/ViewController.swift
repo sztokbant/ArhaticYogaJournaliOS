1e71a4460f74513f483f90c3ff4351a572d272f1
Eduardo Sztokbant <du@du.net.br>
2017-05-06 03:08:26 -0700

    Minor code layout improvements

--- ArhaticYogaJournaliOS/ViewController.swift
+++ ArhaticYogaJournaliOS/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate 
     @IBOutlet var webView: UIWebView!
     @IBOutlet var spinner: UIActivityIndicatorView!
 
-    let allowedDomains: Array<String> = ["arhaticnet.herokuapp.com", "ayj-beta.herokuapp.com", "ayjournal.herokuapp.com", "arhaticyogajournal.com"]
+    let allowedDomains: Array<String> = ["arhaticnet.herokuapp.com",
+                                         "ayj-beta.herokuapp.com",
+                                         "ayjournal.herokuapp.com",
+                                         "arhaticyogajournal.com"]
+
     let defaultUrl: String = "https://arhaticnet.herokuapp.com"
 
     override func viewDidLoad() {
@@ -37,8 +41,9 @@ class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate 
     }
 
     func appendAppVersionToUserAgent() {
-        let shortVersion: String = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-        let userAgent: String = UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent")! + " ArhaticYogaJournaliOS-" + shortVersion
+        let version: String = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+        let userAgent: String =
+            UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent")! + " ArhaticYogaJournaliOS-" + version
         UserDefaults.standard.register(defaults: ["UserAgent": userAgent])
     }
 
@@ -47,12 +52,13 @@ class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate 
         // Dispose of any resources that can be recreated.
     }
 
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest,
+                 navigationType: UIWebViewNavigationType) -> Bool {
         if (request.url?.scheme == "tel") {
             // prevents accidental clicks on numbers from being interpreted as "tel:"
             return false
-        } else if (request.url?.scheme == "mailto"
-            || ((request.url?.scheme == "http" || request.url?.scheme == "https") && !allowedDomains.contains((request.url?.host!)!))) {
+        } else if (request.url?.scheme == "mailto" ||
+            ((request.url?.scheme == "http" || request.url?.scheme == "https") && !allowedDomains.contains((request.url?.host!)!))) {
             if #available(iOS 10.0, *) {
                 UIApplication.shared.open(request.url!, options: [:], completionHandler: nil)
             } else {
@@ -63,7 +69,8 @@ class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate 
         return true
     }
 
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint,
+                                   targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         if (scrollView.contentOffset.y < -100) {
             webView.reload()
         }
